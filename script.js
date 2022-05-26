788f4a848b380cad64f8e6bfe9d2d3e15f488386
นายกานต์ แก้วประภา <88415650+KarnKaewprapa@users.noreply.github.com>
2022-05-26 08:55:33 +0000

    update

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ window.addEventListener("scroll", function(){
     var header = this.document.querySelector("header");
     header.classList.toggle("sticky", this.window.scrollY > 0)
 })
+
+      var i = 0;
+      var colors = [];
+      colors[0] = "white";
+      colors[1] = "hsla(0, 0%, 85%, 1)";
+      function myFunction() {
+         var element = document.getElementById('Dog');
+         element.style.backgroundColor = colors[i];
+         if (i < colors.length-1) {
+             i++;
+           } else {
+              i = 0;
+             }
+        }
